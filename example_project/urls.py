a4d0b8414c84f62ce49f55afa271134d907fdbb9
Manish Lamichhane <m.lamichhane@jobtender24.com>
2019-07-02 10:37:50 +0200

    Fix: Add missing Migration
    - Adds missing migration

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 admin.autodiscover()
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^bouncy/', include('django_bouncy.urls')),
 ]
